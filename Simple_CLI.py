95719fda370807aa459860d4f5510efba18213ea
jeremylaratro <jlaratro24@gmail.com>
2022-08-05 18:50:04 -0400

    Created a simple command line interface with simple text-based input/output, allowing for quick and efficient use.
    Realized Implementations:
    - Startup Options -- SMILES or IUPAC
    - input prompts
    - option to continue translating more
    - option to exit
    - option to switch between translators
    
    Possible plans:
    - Create executable
    - Add more options, IE pull more information such as molecular mass, formula, etc
    - file output (text or csv)

---
+++ Simple_CLI.py
@@ -0,0 +1,49 @@
+from STOUT import translate_forward, translate_reverse
+
+
+
+def SMILES_TR():
+# SMILES to IUPAC name translation
+
+    print("Please enter the SMILES string:")
+    SMILES = input('')
+    IUPAC_name = translate_forward(SMILES)
+    print("IUPAC name of "+SMILES+" is: "+IUPAC_name)
+    print("Completed")
+    print("Translate another?: \n SMILES -> IUPAC (2) \n IUPAC -> SMILES (2) \n Exit (3)")
+    cont = input()
+    if cont == '1':
+        SMILES_TR()
+    if cont == '2':
+        IUPAC_TR()
+    if cont == '3':
+        exit()
+
+
+def IUPAC_TR():
+# IUPAC name to SMILES translation
+    print("Please enter the IUPAC name:")
+    IUPAC = input()
+    smiles_translate = translate_reverse(IUPAC)
+    print("SMILES of "+IUPAC+" is: " + smiles_translate)
+    print("Translate another?: \n IUPAC -> SMILES (1) \n SMILES -> IUPAC (2) \n Exit (3)")
+    cont = input()
+    if cont == '1':
+        SMILES_TR()
+    if cont == '2':
+        IUPAC_TR()
+    if cont == '3':
+        exit()
+
+def choose():
+    print("Would you like to convert: \n (1) SMILES -> IUPAC or \n (2) IUPAC -> SMILES?")
+    choice = input()
+    if choice == '1':
+        SMILES_TR()
+    if choice == '2':
+        IUPAC_TR()
+    else:
+        print('Invalid, please ensure that there are no spaces and try again')
+        return choose()
+choose()
+
