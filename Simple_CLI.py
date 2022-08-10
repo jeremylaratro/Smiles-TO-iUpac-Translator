e381916284311bd657a3c94f5eab64e7aa222292
jeremylaratro <jlaratro24@gmail.com>
2022-08-09 20:31:51 -0400

    Condensed functions, cleaned up code. Added csv output

--- Simple_CLI.py
+++ Simple_CLI.py
@@ -1,24 +1,40 @@
 import csv
-
 from STOUT import translate_forward, translate_reverse
 
 
 outputs = []
-def SMILES_TR():
-# SMILES to IUPAC name translation
 
-    print("Please enter the SMILES string:")
-    SMILES = input('')
-    IUPAC_name = translate_forward(SMILES)
-    print("IUPAC name of "+SMILES+" is: "+IUPAC_name)
-    print("Completed")
-    outputs.append(IUPAC_name)
-    print("Write to text file? \nYes(1) \n No(2)")
-    file_c = input()
-    if file_c == '1':
+# add_output adds the output of each translation to a list
+def add_output(output):
+    outputs.append(output)
+    return outputs
+
+# add_file initiates prompt for writing translations to text, csv, both, or neither
+def add_file():
+    print("Would you like to write to (1) text file, (2) CSV file, (3) no file output, or (4) both?")
+    output_ans = input()
+    if output_ans == '1':
         with open('translations.txt', 'w') as f:
             f.write(', '.join(outputs))
-    print("Translate another?: \n SMILES -> IUPAC (2) \n IUPAC -> SMILES (2) \n Exit (3)")
+            print("File written")
+    if output_ans == '2':
+        with open('translations.csv', 'w') as f:
+            writer = csv.writer(f)
+            writer.writerow(outputs)
+            print("File written")
+    if output_ans == '3':
+        pass
+    if output_ans == '4':
+        with open('translations.txt', 'w') as f:
+            f.write(', '.join(outputs))
+        with open('translations.csv', 'w') as f:
+            writer = csv.writer(f)
+            writer.writerow(outputs)
+            print("File written")
+
+# restart allows user to restart and perform more translations, or exit
+def restart():
+    print("Translate another?: \n SMILES -> IUPAC (1) \n IUPAC -> SMILES (2) \n Exit (3)")
     cont = input()
     if cont == '1':
         SMILES_TR()
@@ -27,7 +43,19 @@ def SMILES_TR():
     if cont == '3':
         exit()
 
+# SMILES_TR prompts user to enter SMILES string and outputs IUPAC name
+def SMILES_TR():
+# SMILES to IUPAC name translation
+    print("Please enter the SMILES string:")
+    SMILES = input('')
+    IUPAC_name = translate_forward(SMILES)
+    print("IUPAC name of "+SMILES+" is: "+ IUPAC_name)
+    print("Completed")
+    add_output(IUPAC_name)
+    add_file()
+    restart()
 
+# IUPAC_TR prompts user to enter IUPAC name and outputs SMILES
 def IUPAC_TR():
 # IUPAC name to SMILES translation
     print("Please enter the IUPAC name:")
@@ -35,22 +63,12 @@ def IUPAC_TR():
     smiles_translate = translate_reverse(IUPAC)
     print("SMILES of "+IUPAC+" is: " + smiles_translate)
     print("Completed")
-    outputs.append(smiles_translate)
-    print("Write to text file? \nYes(1) \n No(2)")
-    file_c = input()
-    if file_c == '1':
-        with open('translations.txt', 'w') as f:
-            f.write(', '.join(outputs))
-    print("Translate another?: \n IUPAC -> SMILES (1) \n SMILES -> IUPAC (2) \n Exit (3)")
-    cont = input()
-    if cont == '1':
-        SMILES_TR()
-    if cont == '2':
-        IUPAC_TR()
-    if cont == '3':
-        exit()
+    add_output(smiles_translate)
+    add_file()
+    restart()
 
-def choose():
+# start prompts user to choose between SMILES to IUPAC or IUPAC to SMILES translation
+def start():
     print("Would you like to convert: \n (1) SMILES -> IUPAC or \n (2) IUPAC -> SMILES?")
     choice = input()
     if choice == '1':
@@ -60,5 +78,6 @@ def choose():
     else:
         print('Invalid, please ensure that there are no spaces and try again')
         return choose()
-choose()
 
+
+start()
